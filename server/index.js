590d18e29256d19a9338be4e26bd073fba07a413
Rahil Desai <you@example.rahildesai.rd99@gmail.com>
2020-12-29 21:54:22 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 const {addUser, removeUser, getUser, getUsersInRoom} = require('./users');
 
 const PORT = process.env.PORT || 5000;
@@ -52,6 +53,7 @@ io.on('connection', (socket) => {
 });
 
 app.use(router);
+app.use(cors());
 
 server.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
